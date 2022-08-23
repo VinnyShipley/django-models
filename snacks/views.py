5389262b351c5a4c4992f001a018503099c15442
Vinny <vinnyshipley@gmail.com>
2022-08-23 15:38:52 -0700

    model working in admin page

--- snacks/views.py
+++ snacks/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+class SnacksListView(ListView):
+  template_name = 'snacks_list.html'
+  model = 
